e15d7bb5b3afd6f04120df976d98d9eda5afb942
Utpal <utpalsen902@gmail.com>
2025-10-06 08:03:56 +0530

    dfdf

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
+import { DM_Sans } from "next/font/google";
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const fontSans = Geist({
+const dmSans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
 });
 
 export default function RootLayout({
@@ -21,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
+      <body className={`${dmSans.variable} font-sans antialiased`}>
         <ClerkProvider>
           <Providers>{children}</Providers>
         </ClerkProvider>
