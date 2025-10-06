de0e427a8e79d0e76d86989df634c3086e98d8de
Utpal <utpalsen902@gmail.com>
2025-10-06 07:37:31 +0530

    dfdf

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -2,7 +2,7 @@ import { AuthGuard } from "@/components/auth-guard";
 import OrganizationGuard from "@/modules/auth/ui/components/organization-guard";
 import { SidebarProvider } from "@workspace/ui/components/sidebar";
 import { cookies } from "next/headers";
-import { DashboardSidebar } from "../components/dashboard-sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
 export const DashboardLayout = async ({
   children,
